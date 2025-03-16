806f2cab932ad4c11970526cb67480c8178f2d5d
Karthikeyan R <2303737720521034@ksrct.net>
2025-03-16 14:58:16 +0530

    Basic React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import About from './pages/About'
 import Home from './pages/Home'
 import Contact from './pages/Contact';
@@ -7,6 +7,11 @@ function App() {
 
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/Home">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
+      </nav>
       <Routes>
         <Route path="/Home" element={<Home/>}/>
         <Route path="/About" element={<About/>}/>
